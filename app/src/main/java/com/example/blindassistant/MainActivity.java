6c77ae6698fe8957c96ebd41dd410333b8d17f55
Hamiduzzaman Noyon <37876041+noyon04@users.noreply.github.com>
2020-04-01 11:53:07 +0600

    Tensorflow Added, Dictionary added.

--- app/src/main/java/com/example/blindassistant/MainActivity.java
+++ app/src/main/java/com/example/blindassistant/MainActivity.java
@@ -2,16 +2,14 @@ package com.example.blindassistant;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -19,15 +17,24 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
-import android.provider.MediaStore;
-import android.provider.Settings;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -42,12 +49,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Timer;
-import java.util.TimerTask;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    String adr, sub_city, city;
+    String adr, sub_city, city,translated1,translated2;
 
     int longt,latt;
     private CardView microid;
@@ -55,6 +61,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextToSpeech mtts;
     private LocationManager locationManager;
     private LocationListener locationListener;
+    private SpeechRecognizer speech ;
+    private FirebaseTranslator translator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +93,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         });
+        FirebaseTranslatorOptions options =
+                new FirebaseTranslatorOptions.Builder()
+                        .setSourceLanguage(FirebaseTranslateLanguage.EN)
+                        .setTargetLanguage(FirebaseTranslateLanguage.BN)
+                        .build();
+        translator =
+                FirebaseNaturalLanguage.getInstance().getTranslator(options);
 
         microid.setOnClickListener(this);
 
@@ -97,35 +112,102 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mtts.setSpeechRate((float) 0.8);
         mtts.speak(str, TextToSpeech.QUEUE_FLUSH, null);
 
+
     }
 
     private void person_input_speak() {
+        checkspeechRecordpermission();
+        speech = SpeechRecognizer.createSpeechRecognizer(MainActivity.this);
+
+
         final Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,"bn-BD");
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,"bn_BD");
         intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,this.getPackageName());
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
         intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,3);
-        intent.putExtra("android.speech.extra.EXTRA_ADDITIONAL_LANGUAGES",new String[]{"bn-BD"});
+        intent.putExtra("android.speech.extra.EXTRA_ADDITIONAL_LANGUAGES",new String[]{"bn_BD"});
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "bn-BD");
         intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS,3000);
         intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS,3000);
-        try {
+        intent.putExtra("android.intent.extra.durationLimit",3);
+        speech.setRecognitionListener(new listener());
 
-            Handler handler=new Handler();
-            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
-            Runnable r =new Runnable() {
-                @Override
-                public void run() {
+        speech.startListening(intent);
+        new CountDownTimer(5000,1000){
 
-                }
-            };
-           // handler.postDelayed(r,3000);
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                speech.stopListening();
+            }
+        }.start();
+
+    }
+    class listener implements RecognitionListener{
+
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            Toast.makeText(MainActivity.this,"Start Listening",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            Toast.makeText(MainActivity.this,"Stop Listening",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onError(int error) {
+            if(error==7)
+                mobile_speak("দুঃখিত কিছু বুঝা যায় নি");
+            else if(error==5){
+                Toast.makeText(MainActivity.this,"Error Listening"+error,Toast.LENGTH_LONG).show();
+            }
 
 
+        }
 
-        } catch (Exception e) {
-            mobile_speak("আপনার ডাটা অথবা ইন্টারনেট অন করুন");
-            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        @Override
+        public void onResults(Bundle results) {
+            ArrayList<String> matches=results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            if(matches!=null){
+                processResult(matches.get(0).trim());
+                //Toast.makeText(MainActivity.this,""+matches.get(0).trim(),Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+
+        }
+    }
+
+    private void checkspeechRecordpermission() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.RECORD_AUDIO},1);
         }
     }
 
@@ -207,27 +289,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         String content;
         Weather weather = new Weather();
+
+
         try {
             content = weather.execute("https://openweathermap.org/data/2.5/weather?lat="+latt+"&lon="+longt+"&appid=b6907d289e10d714a6e88b30761fae22").get();
-            Toast.makeText(this,content,Toast.LENGTH_LONG).show();
+            //Toast.makeText(this,content,Toast.LENGTH_LONG).show();
             //Log.i("contentdata",content);
 
             JSONObject jsonObject = new JSONObject(content);
             String weatherData = jsonObject.getString("weather");
             String mainTemperature = jsonObject.getString("main");
-            Toast.makeText(this,weatherData,Toast.LENGTH_LONG).show();
+            //Toast.makeText(this,weatherData,Toast.LENGTH_LONG).show();
 
             // Log.i("weatherdata",weatherData);
 
             JSONArray array = new JSONArray(weatherData);
 
-            String main="";
+            String main = "";
             String description="";
             String temperature ="";
-            String city_Name = "";
+            final String city_Name = "";
             for (int i=0; i<array.length();i++){
                 JSONObject weatherpart = array.getJSONObject(i);
-                main= weatherpart.getString("main");
+                main = weatherpart.getString("main");
                 description = weatherpart.getString("description");
             }
             JSONObject mainpart = new JSONObject(mainTemperature);
@@ -235,8 +319,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             double temp_int = Double.parseDouble(temperature);
             temp_int = Math.round(temp_int);
             int t_value = (int) temp_int;
-            String resultText = "আকাশ: "+main+"\n বর্ণনা: "+description+"\n তাপমাত্রা: "+t_value+" degree Celsius";
-            mobile_speak(resultText);
+
+            translate(main,description);
+
+
+            final String[] finalMain = {null};
+
+            final String[] finalDescription = {null};
+            Handler handler=new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+               public void run() {
+                    finalMain[0] = translated1;
+                    finalDescription[0] =translated2;
+
+                    Toast.makeText(MainActivity.this,"আকাশ: "+ finalMain[0]+"বর্ণনা: "+ finalDescription[0],Toast.LENGTH_LONG).show();
+                    String resultText = "আকাশ: "+ finalMain[0] +"\n বর্ণনা: "+ finalDescription[0] +"\n তাপমাত্রা: "+t_value+" degree Celsius";
+                    mobile_speak(resultText);
+
+               }
+            },2000);
+
+
             //Toast.makeText(this,"Temperature"+resultText,Toast.LENGTH_LONG).show();
         } catch (Exception e) {
             e.printStackTrace();
@@ -322,6 +426,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(this, permissions[0], Toast.LENGTH_SHORT).show();
                 locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
             }
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, permissions[0], Toast.LENGTH_SHORT).show();
+
+
+            }
         }
 
     }
@@ -344,17 +453,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
     }
+
     private void processResult(String command)
     {
-        //Toast.makeText(MainActivity.this,"("+command+") "+command.indexOf("সময়")+" "+command.equals("সময়"),Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this,""+command,Toast.LENGTH_LONG).show();
+        for(int i=0;i<command.length();i++){
+            if(command.charAt(i)== 2479 && command.charAt(i+1)== 2492){
+                StringBuilder command2=new StringBuilder(command);
+                command2.deleteCharAt(i+1);
+                command=command2.toString();
+                break;
+            }
+        }
 
-       //int x=command.charAt(2);
-            //Toast.makeText(MainActivity.this, "(" + command.charAt(2) + ") "+x+" ", Toast.LENGTH_SHORT).show();
-
-
-
-        //int a=(int)'স',b=(int)'ম',cc=(int)'য়';
-        //Toast.makeText(MainActivity.this, "(" +a+". "+b+", "+cc, Toast.LENGTH_SHORT).show();
         if(command.indexOf("সময়")!=-1 || command.indexOf("সময")!=-1 ||command.indexOf("টাইম")!=-1)
         {
             String currentTime = new SimpleDateFormat("hh:mm aa", Locale.getDefault()).format(new Date());
@@ -378,19 +489,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             },2000);
 
 
-        } else if (command.indexOf("কেমেরা")!=-1||command.indexOf("ক্যামেরা")!=-1)
+        } else if (command.indexOf("পরিবেশ")!=-1||command.indexOf("এনভাযরনমেন্ট")!=-1)
         {
             Intent intent2=new Intent(MainActivity.this,CameraActivity.class);
             startActivity(intent2);
             //Intent intent2=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             //startActivity(intent2);
 
-        }else if (command.indexOf("ওয়েদার ")!=-1 || command.indexOf("টেম্পারেচার")!=-1||command.indexOf("আবহাওয়া ")!=-1){
+        }else if (command.indexOf("ওযেদার")!=-1 || command.indexOf("টেম্পারেচার")!=-1||command.indexOf("আবহাওযা")!=-1){
             location_service();
+            FirebaseApp.initializeApp(MainActivity.this);
             final Handler handler=new Handler();
             handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
+                    mobile_speak("একটু অপেক্ষা করুন");
                     weather_service();
                 }
             },2000);
@@ -405,7 +518,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             },2000);
 
-        } else if (command.indexOf("close")!=-1||command.indexOf("exit")!=-1)
+        } else if (command.indexOf("ব্যাটারি চার্জ")!=-1 || command.indexOf("ফোনের চার্জ")!=-1|| command.indexOf("ব্যাটারি পার্সেন্টেজ")!=-1|| command.indexOf("চার্জ")!=-1|| command.indexOf("পার্সেন্টেজ")!=-1){
+            getBattery_percentage();
+
+        }else if (command.indexOf("close")!=-1||command.indexOf("exit")!=-1)
         {
             android.os.Process.killProcess(android.os.Process.myPid());
             System.exit(1);
@@ -413,6 +529,97 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
 
+    }
+
+    private void translate(String inputtext1,String inputtext2) {
+
+      /*Install Firebase Model English
+        FirebaseModelManager modelManager = FirebaseModelManager.getInstance();
+        FirebaseTranslateRemoteModel frModel =
+                new FirebaseTranslateRemoteModel.Builder(FirebaseTranslateLanguage.BN).build();
+        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
+                .requireWifi()
+                .build();
+        modelManager.downloadRemoteModelIfNeeded(frModel)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void v) {
+                        Log.d("download","bdmodel");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Error.
+                    }
+                });
+
+        Install Firebase Model Bangla
+        FirebaseTranslateRemoteModel frModel2 =
+                new FirebaseTranslateRemoteModel.Builder(FirebaseTranslateLanguage.EN).build();
+        FirebaseModelDownloadConditions conditions2 = new FirebaseModelDownloadConditions.Builder()
+                .requireWifi()
+                .build();
+        modelManager.downloadRemoteModelIfNeeded(frModel2)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void v) {
+                        Log.d("download","enmodel");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Error.
+                    }
+                });*/
+        final String[] inputText = {inputtext1,inputtext2};
+
+        translator.translate(inputText[0])
+                .addOnSuccessListener(
+                        new OnSuccessListener<String>() {
+                            @Override
+                            public void onSuccess(@NonNull String translatedText) {
+                               translated1=translatedText;
+                               //Toast.makeText(MainActivity.this,"tranlate"+inputText2[0],Toast.LENGTH_LONG).show();
+
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this,"exception"+e.getMessage(),Toast.LENGTH_LONG).show();
+                            }
+                        });
+        translator.translate(inputText[1])
+                .addOnSuccessListener(
+                        new OnSuccessListener<String>() {
+                            @Override
+                            public void onSuccess(@NonNull String translatedText) {
+                                translated2=translatedText;
+                                //Toast.makeText(MainActivity.this,"tranlate"+inputText2[0],Toast.LENGTH_LONG).show();
+
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this,"exception"+e.getMessage(),Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+    }
+
+    private void getBattery_percentage() {
+        IntentFilter ifilter=new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus=getApplicationContext().registerReceiver(null,ifilter);
+        int level=batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL,-1);
+        int scale=batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE,-1);
+        float batterPCT=level/(float)scale;
+        float p=batterPCT*100;
+        mobile_speak("আপনার ফোনের চার্জ"+String.valueOf(Math.round(p))+"পার্সেন্টেজ");
     }
 
     @Override
